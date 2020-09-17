85a5da07ce87b8b2e5b38e64332bf34dae9fe8c6
Hyunwoo Shim <shim.hw0810@gmail.com>
2020-09-17 21:01:06 +0900

    Hide contentbar

--- api/spotify.py
+++ api/spotify.py
@@ -93,7 +93,7 @@ def makeSVG(data):
     barCSS = barGen(barCount)
 
     if data == {} or data["item"] == "None":
-        # contentBar = "" #Shows/Hides the EQ bar if no song is currently playing
+        contentBar = "" #Shows/Hides the EQ bar if no song is currently playing
         currentStatus = "Was playing:"
         recentPlays = recentlyPlayed()
         recentPlaysLength = len(recentPlays["items"])
