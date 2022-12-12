05cc554ca6991477b499cb3e61833671aa081a6c
Alexander Hamilton <65139378+GalexY727@users.noreply.github.com>
2022-12-11 17:37:25 -0800

    Fix syntax error on itemIndex declaration

--- api/spotify.py
+++ api/spotify.py
@@ -110,7 +110,7 @@ def makeSVG(data, background_color, border_color):
         currentStatus = "Last played:"
         recentPlays = recentlyPlayed()
         recentPlaysLength = len(recentPlays["items"])
-        itemIndex = random.randint(0)#, recentPlaysLength - 1)
+        itemIndex = random.randint(0,0)#, recentPlaysLength - 1)
         item = recentPlays["items"][itemIndex]["track"]
     else:
         item = data["item"]
